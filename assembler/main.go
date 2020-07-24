3cf586b8a734af14c112e9e3f63fbba461626952
HagaSpa <justicesparrow13@gmail.com>
2020-07-24 17:59:35 +0900

    [WIP]Impl main()
    Up to binary generation

--- assembler/main.go
+++ assembler/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"assembler/code"
+	"assembler/parser"
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello")
+	// parse args
+	flag.Parse()
+	flags := flag.Args()
+	if flags == nil {
+		// TODO: err or start repl?
+		os.Exit(1)
+	}
+
+	// open assenbly
+	fp, err := os.Open(flags[0])
+	if err != nil {
+		os.Exit(1)
+	}
+	s := bufio.NewScanner(fp)
+
+	// generate binary
+	var b []string
+	p := parser.New(s)
+	for p.HasMoreCommands() {
+		p.Advance()
+		if p.Type == "" {
+			continue
+		}
+		c := code.New(p)
+		b = append(b, c.Binary)
+	}
+
+	fmt.Printf("%s \n", b)
+
+	defer fp.Close()
 }
